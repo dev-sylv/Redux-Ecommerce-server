2c60f8a62106777c431a908db51fb3f974241be7
nicsylvia <nicsylvia15f@gmail.com>
2023-02-17 14:13:02 -0800

    saved

--- Controller/AllController.ts
+++ Controller/AllController.ts
@@ -40,3 +40,31 @@ router.post("/register", async (req: Request, res: Response) => {
 		});
 	}
 });
+
+router.post("/register", async (req: Request, res: Response) => {
+	try {
+		const { name, email, password } = req.body;
+
+		// const salt = await bcrypt.genSalt(10);
+		// const hash = bcrypt.hash(password, salt);
+		const createUser = await UserModel.create({
+			name,
+			email,
+			password,
+			isAdmin: false,
+		});
+		return res.status(200).json({
+			message: "success",
+			data: createUser,
+			// token: jwt.sign(
+			// 	{ _id: createUser._id },
+			// 	"Ths-57aenrn-53q4yhnae-05q3ujn",
+			// 	{ expiresIn: "1d" },
+			// ),
+		});
+	} catch (err) {
+		res.status(404).json({
+			message: "an error occured",
+		});
+	}
+});
